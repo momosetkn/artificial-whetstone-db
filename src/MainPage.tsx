04c6ae0be2b4841ec8e19b034e90b10e988034ef
momosetkn <hyakkun@gmail.com>
2021-09-12 00:41:28 +0900

    Can search table

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,21 +1,64 @@
-import React, {useState} from 'react';
-import {InputGroup, NumberRange, RangeSlider} from "@blueprintjs/core";
+import React, {useMemo, useState} from 'react';
+import {HTMLTable, InputGroup, NumberRange, RangeSlider} from "@blueprintjs/core";
 import styled from "styled-components";
+import {Product, products} from "./data/products";
+import {companies, Company} from "./data/companies";
 
 type State = {
   freeWord: string;
   gridRange: NumberRange;
 };
 
-const grids = ["#0", "#100", "#200", "#300", "#400", "#500", "#600", "#800", "#1,000", "#3,000", "#6,#000", "#8,000", "もっと！"];
+const grids = [
+  {label: "#0", value: 0},
+  {label: "#100", value: 100},
+  {label: "#200", value: 200},
+  {label: "#300", value: 300},
+  {label: "#400", value: 400},
+  {label: "#500", value: 500},
+  {label: "#600", value: 600},
+  {label: "#800", value: 800},
+  {label: "#1,000", value: 1_000},
+  {label: "#3,000", value: 2_000},
+  {label: "#3,000", value: 3_000},
+  {label: "#6,000", value: 6_000},
+  {label: "#8,000", value: 8_000},
+  {label: "もっと！", value: Number.MAX_VALUE},
+] as const;
+
+const gridLabels: string[] = grids.map(x => x.label);
+const gridValues: number[] = grids.map(x => x.value);
 
 const initialState: State = {
   freeWord: '',
   gridRange: [0, grids.length - 1],
 };
 
+const companiesMap: Record<string, Company> = companies.reduce((acc, cur) => ({
+  ...acc,
+  [cur.name]: cur
+}), {});
+
 export const MainPage = () => {
   const [state, update] = useState<State>(initialState);
+
+  const selectedGridRangesValues: [number, number] = useMemo(() => [gridValues[state.gridRange[0]], gridValues[state.gridRange[1]]], [state.gridRange]);
+  const filteredProducts = useMemo(() => products.filter(product => {
+    const isTargetGird = (value: Product["grid"]) => {
+      // TODO: 番手情報が無いものは、とりあえず無条件に出しておく
+      if (!value) return true;
+      if (typeof value === "number" && selectedGridRangesValues[0] <= value && value <= selectedGridRangesValues[1]) return true;
+      if (typeof value === "string") {
+        const matchGroups = value.match(/(\d+)/g);
+        for (const m in matchGroups) {
+          const n = Number(m);
+          if (selectedGridRangesValues[0] <= n && n <= selectedGridRangesValues[1]) return true;
+        }
+      }
+    }
+    return (!state.freeWord || product.freeWords.search(state.freeWord) !== -1)
+      && isTargetGird(product.grid);
+  }), [state, selectedGridRangesValues])
 
   return (
     <Main>
@@ -31,14 +74,71 @@ export const MainPage = () => {
           min={0}
           max={grids.length - 1}
           stepSize={1}
-          labelRenderer={(value, opts) => grids[value]}
+          labelRenderer={(value, opts) => gridLabels[value]}
           onChange={(e) => update(prev => ({...prev, gridRange: e}))}
           value={state.gridRange}
         />
       </StyledControls>
+      <DataTables items={filteredProducts} />
     </Main>
   );
 };
+
+const DataTables = ({items}: { items: Product[] }) => {
+  return (
+    <StyledTableContainer>
+      <StyledHTMLTable striped>
+        <thead>
+        <StyledStickyTr>
+          <th>会社名</th>
+          <th>品番</th>
+          <th>商品名</th>
+          <th>粒度</th>
+          <th>製法</th>
+          <th>砥粒</th>
+          <th>サイズ</th>
+          <th>金額</th>
+          <th>URL</th>
+          <th>備考</th>
+        </StyledStickyTr>
+        </thead>
+        <tbody>
+        {items.map(item => (
+          <tr key={item.id}>
+            <StyledTd><a href={companiesMap[item.company].url} target="_blank"
+                         rel="noreferrer"> {item.company}</a></StyledTd>
+            <StyledTd>{item.productNumber}</StyledTd>
+            <StyledTd>{item.productName}</StyledTd>
+            <StyledTd>{item.grid}</StyledTd>
+            <StyledTd>{item.manufacturingMethod}</StyledTd>
+            <StyledTd>{item.abrasiveGrains}</StyledTd>
+            <StyledTd>{item.size}</StyledTd>
+            <StyledTd>{item.price}</StyledTd>
+            <StyledTd><a href={item.url} target="_blank" rel="noreferrer">{item.url}</a></StyledTd>
+            <StyledTd>
+            <pre>
+              <code>
+                {[item.remarks, item.remarks2].filter(x => x).join("\n")}
+              </code>
+            </pre>
+            </StyledTd>
+          </tr>
+        ))}
+        </tbody>
+        <tfoot>
+        <tr>
+          <td>Total</td>
+          <td>{items.length}</td>
+        </tr>
+        </tfoot>
+      </StyledHTMLTable>
+    </StyledTableContainer>
+  )
+};
+
+const controlsHeight = 40;
+const mainPadding = 24;
+const dataTablesMarginTop = 16;
 
 const Main = styled.div`
   padding: 24px;
@@ -47,8 +147,34 @@ const Main = styled.div`
 const StyledControls = styled.div`
   display: flex;
   width: 100%;
-  
+
   & > * + * {
     margin-left: 24px;
   }
+`;
+
+const StyledTableContainer = styled.div`
+  height: calc(100vh - ${mainPadding*2 + controlsHeight + dataTablesMarginTop}px);
+  overflow-y: scroll;
+  margin-top: ${dataTablesMarginTop}px;
+`;
+
+const StyledHTMLTable = styled(HTMLTable)`
+  table-layout: fixed;
+  width: 100%;
+`;
+
+const StyledStickyTr = styled.tr`
+  position: sticky;
+  top: 0;
+  background: white;
+  & th {
+    width: 10%;
+  }
+`;
+
+const StyledTd = styled.td`
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
 `;
---
+++ src/data/companies.ts
@@ -0,0 +1,139 @@
+export type Company = {
+  name: string,
+  url?: string,
+}
+
+export const companies =  [
+  {
+    name: "シャプトン",
+    url: "https://shapton.co.jp/"
+  },
+  {
+    name: "ナニワ",
+    url: "https://www.naniwa-kenma.co.jp/"
+  },
+  {
+    name: "ツボ万",
+    url: ""
+  },
+  {
+    name: "研承",
+    url: "https://tsukiyama.buyshop.jp/"
+  },
+  {
+    name: "キング砥石株式会社(松永トイシ)",
+    url: "http://matsunaga-corp.co.jp/"
+  },
+  {
+    name: "朝日虎(松永トイシ)",
+    url: "http://matsunaga-corp.co.jp/"
+  },
+  {
+    name: "中京研磨(松永トイシ)",
+    url: "http://matsunaga-corp.co.jp/"
+  },
+  {
+    name: "中京研磨株式会社(松永トイシ)",
+    url: "http://matsunaga-corp.co.jp/"
+  },
+  {
+    name: "中京研磨(松永トイシ)",
+    url: "http://matsunaga-corp.co.jp/"
+  },
+  {
+    name: "朝日虎(松永トイシ)",
+    url: "http://matsunaga-corp.co.jp/"
+  },
+  {
+    name: "ノーブランド？(松永トイシ)",
+    url: "http://matsunaga-corp.co.jp/"
+  },
+  {
+    name: "株式会社ノリタケカンパニーリミテド(松永トイシ)",
+    url: "http://matsunaga-corp.co.jp/"
+  },
+  {
+    name: "サイコム",
+    url: "http://www.saicom.info/"
+  },
+  {
+    name: "スエヒロ",
+    url: "https://www.suehiro-toishi.com/"
+  },
+  {
+    name: "實光刃物",
+    url: "https://www.jikko.jp/"
+  },
+  {
+    name: "今西製砥",
+    url: "https://home.etown.ne.jp/"
+  },
+  {
+    name: "大谷砥石",
+    url: "https://hamono.ocnk.net/"
+  },
+  {
+    name: "田中砥石 ",
+    url: ""
+  },
+  {
+    name: "京東山砥石",
+    url: "https://www.honmamon.jp/"
+  },
+  {
+    name: "NSK",
+    url: "https://nskdiatoishi.jp/"
+  },
+  {
+    name: "ノーブランド",
+    url: "https://hamono.ocnk.net/"
+  },
+  {
+    name: "伊予鉱業所",
+    url: ""
+  },
+  {
+    name: "森平",
+    url: "https://sankeishop.jp/"
+  },
+  {
+    name: "宮越製砥",
+    url: "https://shop.toishiya-miyagoshi.com/"
+  },
+  {
+    name: "アイウッド",
+    url: "https://www.monotaro.com/"
+  },
+  {
+    name: "大和製砥所",
+    url: "http://www.yamatoseito.co.jp/"
+  },
+  {
+    name: "SK11(藤原産業)",
+    url: "http://www.fujiwarasangyo-markeweb2.com/"
+  },
+  {
+    name: "三京ダイヤモンド工業",
+    url: "https://www.sankyo-diamond.co.jp/"
+  },
+  {
+    name: "柳瀬",
+    url: "https://yanase-saving.com/"
+  },
+  {
+    name: "鳳凰？",
+    url: "http://www.echigo-douraku.com/"
+  },
+  {
+    name: "ノーブランド？",
+    url: "https://www.ehamono.com/"
+  },
+  {
+    name: "堺一文字光秀",
+    url: "https://www.ichimonji.co.jp/"
+  },
+  {
+    name: "砥石どっとこむ",
+    url: "http://www.e-toishi.com/"
+  },
+];
