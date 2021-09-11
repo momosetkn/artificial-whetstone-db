7bdd442307f76eefbcae18d632a5e62f2866fce7
momosetkn <hyakkun@gmail.com>
2021-09-12 01:03:39 +0900

    Add tooltip, Add volume

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -3,6 +3,7 @@ import {HTMLTable, InputGroup, NumberRange, RangeSlider} from "@blueprintjs/core
 import styled from "styled-components";
 import {Product, products} from "./data/products";
 import {companies, Company} from "./data/companies";
+import {Tooltip2} from "@blueprintjs/popover2";
 
 type State = {
   freeWord: string;
@@ -97,33 +98,14 @@ const DataTables = ({items}: { items: Product[] }) => {
           <th>製法</th>
           <th>砥粒</th>
           <th>サイズ</th>
+          <th>容積</th>
           <th>金額</th>
           <th>URL</th>
           <th>備考</th>
         </StyledStickyTr>
         </thead>
         <tbody>
-        {items.map(item => (
-          <tr key={item.id}>
-            <StyledTd><a href={companiesMap[item.company].url} target="_blank"
-                         rel="noreferrer"> {item.company}</a></StyledTd>
-            <StyledTd>{item.productNumber}</StyledTd>
-            <StyledTd>{item.productName}</StyledTd>
-            <StyledTd>{item.grid}</StyledTd>
-            <StyledTd>{item.manufacturingMethod}</StyledTd>
-            <StyledTd>{item.abrasiveGrains}</StyledTd>
-            <StyledTd>{item.size}</StyledTd>
-            <StyledTd>{item.price}</StyledTd>
-            <StyledTd><a href={item.url} target="_blank" rel="noreferrer">{item.url}</a></StyledTd>
-            <StyledTd>
-            <pre>
-              <code>
-                {[item.remarks, item.remarks2].filter(x => x).join("\n")}
-              </code>
-            </pre>
-            </StyledTd>
-          </tr>
-        ))}
+          {items.map(item => <Row key={item.id}  item={item}/>)}
         </tbody>
         <tfoot>
         <tr>
@@ -136,12 +118,60 @@ const DataTables = ({items}: { items: Product[] }) => {
   )
 };
 
+const Row = ({item}: { item: Product }) => {
+  const remarks = [item.remarks, item.remarks2].filter(x => x).join("\n");
+  const volume = (() => {
+    const [x1, x2, x3] = item.size.split(/\D+/);
+    return (Number(x1) || 0) * (Number(x2) || 0) * (Number(x3) || 0);
+  })();
+  return (
+    <tr>
+      <StyledTd><a href={companiesMap[item.company].url} target="_blank"
+                   rel="noreferrer"> {item.company}</a></StyledTd>
+      <StyledTd>{item.productNumber}</StyledTd>
+      <StyledTd>
+        <Tooltip2 content={item.productName}>
+          {item.productName}
+        </Tooltip2>
+      </StyledTd>
+      <StyledTd>{typeof item.grid === 'number' ? `#${item.grid.toLocaleString()}` : item.grid}</StyledTd>
+      <StyledTd>
+        <Tooltip2 content={item.manufacturingMethod}>
+          {item.manufacturingMethod}
+        </Tooltip2>
+      </StyledTd>
+      <StyledTd>
+        <Tooltip2 content={item.abrasiveGrains}>
+         {item.abrasiveGrains}
+        </Tooltip2>
+      </StyledTd>
+      <StyledTd>{item.size}</StyledTd>
+      <StyledTd>{`${volume.toLocaleString()}mm³`}</StyledTd>
+      <StyledTd>{item.price}</StyledTd>
+      <StyledTd>
+        <Tooltip2 content={item.url}>
+          <a href={item.url} target="_blank" rel="noreferrer">{item.url}</a>
+        </Tooltip2>
+      </StyledTd>
+      <StyledTd>
+        <Tooltip2 content={remarks}>
+          <pre>
+            <code>
+              {remarks}
+            </code>
+          </pre>
+        </Tooltip2>
+      </StyledTd>
+    </tr>
+  );
+};
+
 const controlsHeight = 40;
 const mainPadding = 24;
 const dataTablesMarginTop = 16;
 
 const Main = styled.div`
-  padding: 24px;
+  padding: 64px;
 `;
 
 const StyledControls = styled.div`
