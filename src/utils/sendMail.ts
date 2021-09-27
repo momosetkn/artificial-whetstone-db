5c320fb27022b4f65cb4cf4d31f5d377b7920018
momosetkn <hyakkun@gmail.com>
2021-09-27 12:23:34 +0900

    Fix sendMail

--- src/utils/sendMail.ts
+++ src/utils/sendMail.ts
@@ -1,10 +1,7 @@
 export const sendMail = async (params:{title: string, message: string}) => {
-  const res = await fetch(process.env.SEND_MAIL_ENDPOINT || "", {
+  await fetch(process.env.SEND_MAIL_ENDPOINT || "", {
     method: "POST",
     body: JSON.stringify(params),
-    mode: "cors",
-    credentials: "include",
-  }).then(x => x.json());
-
-  console.log(res);
+    mode: "no-cors",
+  });
 };
