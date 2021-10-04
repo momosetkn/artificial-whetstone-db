97597b3c20605ea105a5ba3f161f2a5d4dca1853
momosetkn <hyakkun@gmail.com>
2021-10-04 18:04:45 +0900

    Fix package.json

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import reportWebVitals from './reportWebVitals';
 import { MainPage } from "./MainPage";
 import './index.css';
 import 'normalize.css/normalize.css';
@@ -26,8 +25,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
